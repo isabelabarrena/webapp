681128ecb72a63d02ba7375b867363c7bd021a71
isabelabarrena <isabelbarrenaaliaga8@gmail.com>
2025-01-08 18:49:00 +0100

    modooscuroanadido

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// ANIMACION FADEIN
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll(".fade-in");
 
@@ -16,4 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
   sections.forEach((section) => {
     observer.observe(section);
   });
+});
+// BOTON MODO OSCURO
+const themeToggleButton = document.getElementById("theme-toggle");
+
+// Función para cambiar el tema
+themeToggleButton.addEventListener("click", () => {
+  // Alternar la clase dark-theme en el body y header
+  document.body.classList.toggle("dark-theme");
+  document.querySelector("header").classList.toggle("dark-theme");
+
+  // Cambiar el texto del botón dependiendo del tema actual
+  if (document.body.classList.contains("dark-theme")) {
+    themeToggleButton.textContent = "Modo Claro"; // Cambiar el texto del botón a "Modo Claro"
+  } else {
+    themeToggleButton.textContent = "Modo Oscuro"; // Cambiar el texto del botón a "Modo Oscuro"
+  }
 });
